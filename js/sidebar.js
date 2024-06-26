dc03ff2700a75d9738212b8829d34d6d522934d1
Alexis Castillo <alexis.ponce951@gmail.com>
2024-06-25 21:55:35 -0600

    Update CSS styling and enhance navigation animations
    A significant number of CSS changes were made to improve the layout and appearance of the website. Changes include adjustments to margins, font sizes, display settings, and adding new CSS for "additional skills" section. Additionally, jQuery animations were enhanced on the sidebar navigation links for a better user interaction experience. An "additional skills" content segment has also been inserted into the index.html.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,26 +1,37 @@
-$(document).ready(function () {
+$(function () {
     // Instancia de la barra lateral
     const sidebar = $("#sidebar");
+
+    // Se agrega el callback para remover las clases una vez que termine la animación.
+    $("#about_me").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $(this).removeClass('animate__animated animate__fadeInDown');
+    });
 
     // Mostrar el menú al hacer clic
     $("#toggle-btn").on('click', function (){
         sidebar.toggleClass("expand");
     });
 
-    // Ocultar todas las secciones excepto 'Acerca de mí'
-    $(".articles__cv").hide();
-    $("#about_me").show();
-
     // Evento para mostrar las secciones al hacer clic en los enlaces
     $(".link__mostrar").on('click', function(){
 
-        // Ocultar todas las secciones
-        $(".articles__cv").hide();
-
         // Mostrar la sección correspondiente al enlace clickeado
         const navigation = $(this).attr("href");
-        $(navigation).show();
+        // CONTROL PARA ABOUT ME
+        let animacion = "animate__fadeIn";
+        if(navigation === "#about_me"){
+            animacion = "animate__fadeInDown";
+        }
 
+        // Asegúrate de que las clases de animación no estén activas antes de agregarlas
+        $(navigation).removeClass(`animate__animated ${animacion}`);
+        // Agrega las clases de animación
+        $(navigation).addClass(`animate__animated ${animacion}`);
+        // Una vez que la animación haya terminado, eliminar las clases.
+        $(navigation).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+        function() {
+            $(navigation).removeClass(`animate__animated ${animacion}`);
+        });
         // Si la barra lateral está expandida, colapsarla
         if(sidebar.hasClass("expand")) {
             sidebar.removeClass("expand");
